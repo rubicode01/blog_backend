75616b5db3711777b4e00ff51f470249dab09c7b
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-18 16:27:19 +0200

    Create Routes for post requests

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -3,6 +3,18 @@ import { getAllPosts } from "../controllers/postsController.js";
 
 const postsRouter = Router();
 
-postsRouter.route("/").get(getAllPosts);
+postsRouter.route("/")
+    .get(getAllPosts)
+    .post(createPost);
+
+postsRouter.route("/:id")
+    .get(getSinglePost) //Einzelner Post + dazugehörige Kommentare
+    .put(updateSinglePost) 
+
+postsRouter.route("/:id/comments")
+    .post(createComment)
+
+postsRouter.route("/:id/likes")
+    .post(likePost) //Karims Part
 
 export default postsRouter;
