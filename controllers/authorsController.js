ba5824dd8be3f1b31de796a3bbee70ee34768fe1
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-19 16:52:40 +0200

    Create getAllAuthors endpoint

--- controllers/authorsController.js
+++ controllers/authorsController.js
@@ -2,7 +2,7 @@ import pool from "../db/pg.js";
 
 export const getAllAuthors = (req, res) => {
 
-    pool.query()
-    .then()
-    .catch()
+    pool.query("SELECT * FROM authors")
+    .then((data) => res.status(200).json(data.rows))
+    .catch((err) => res.status(500).json(err))
 }
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/posts", postsRouter);
-app.use("api/authors", authorsRouter);
+app.use("/api/authors", authorsRouter);
 
 app.listen(port, () => 
     console.log(`Server listen on port ${port}`)
